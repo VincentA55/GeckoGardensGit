1f66afeef5f83f490cd08445b4cbc9edbad24144
Vincent A. <71930091+VincentA55@users.noreply.github.com>
2025-03-21 22:01:04 +0000

    added sit spin wander

--- geckogardensv1/Characters/NewGeck.gd
+++ geckogardensv1/Characters/NewGeck.gd
@@ -12,6 +12,11 @@ enum States {
 	Dead
 }
 var state : States = States.Neutral
+var stateString : String 
+# Gecko's action weights
+var Spin: int = 3
+var Sit: int = 5
+var Wander: int = 3
 
 @export var walkSpeed : float = 7.0
 @export var runSpeed : float = 10.0
@@ -31,6 +36,10 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if state == States.Walking:
 		move_to_location(delta)
+		
+		
+	stateString = States.keys()[state]#for debugging and billboard
+	$Billboard._update()
 
 
 func _on_navigation_finished() -> void:
@@ -59,26 +68,60 @@ func move_to_location(delta:float)->void:
 	velocity = direction * walkSpeed
 	move_and_slide() 
 
-#This is used the moment a state is changed to prevent the action happening every frame
-func ChangeState(newState : States) -> void:
-	state = newState
-	match state:
-		States.Neutral:
-			$WanderTimer.start()
-			
-			
-			return
-		States.Walking: 
-			pass
-
-
-func _on_wander_timer_timeout() -> void:
-	if state == States.Neutral:
-		ChangeState(States.Walking)
-		get_random_position()
-
 func get_random_position()->void:
 	var random_position := Vector3.ZERO
 	random_position.x = randf_range(-9,15.0)
 	random_position.z = randf_range(-9,15.0)
 	navigation_agent.set_target_position(random_position)
+	
+#This is used the moment a state is changed to prevent the action happening every frame
+func ChangeState(newState : States) -> void:
+	state = newState
+	match state:
+		States.Neutral:
+			$AnimationPlayer.play("RESET")
+			$WanderTimer.start()
+			
+			
+			return
+		States.Walking: 
+			$AnimationPlayer.play("wander")
+			pass
+
+#This is when it makes the "decision" to do next
+func _on_wander_timer_timeout() -> void:
+	if state == States.Neutral:
+		perform_wander_action()
+
+#Recieves the choice as a string and does it
+func perform_wander_action():
+	var action = choose_wander_action()
+	print("Gecko chose: ", action)  # Debugging
+
+	if action == "spin":
+		$AnimationPlayer.play("spin")
+		$WanderTimer.start()
+	elif action == "sit":
+		$AnimationPlayer.play("sit")
+		$WanderTimer.start()
+	elif action == "wander":
+		ChangeState(States.Walking)
+		get_random_position()
+	else:
+		#$AnimationPlayer.play("idle")  
+		pass
+		
+#using math and numbers to choose return an actions string
+func choose_wander_action() -> String:
+	var total = Spin + Sit + Wander  # Get total weight 
+	if total == 0:
+		return "idle"  # No valid actions  
+
+	var r = randi_range(1, total)  # Pick random number between 1 and total 
+
+	if r <= Spin:
+		return "spin"  
+	elif r <= Spin + Sit:
+		return "sit"  
+	else:
+		return "wander" 
