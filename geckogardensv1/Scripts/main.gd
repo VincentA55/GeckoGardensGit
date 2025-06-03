418d87b51ef4cde5f96bba1cfb36c6de92f4d674
Vincent A. <71930091+VincentA55@users.noreply.github.com>
2025-06-03 22:34:31 +0100

    misc

--- geckogardensv1/Scripts/main.gd
+++ geckogardensv1/Scripts/main.gd
@@ -12,14 +12,14 @@ extends Node
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
-		var food = food_manager.add_food(2)  # Register food in FoodManager
-		food.global_position = get_viewport().get_camera_3d().project_position(get_viewport().get_mouse_position(), 10)
-
-		#Connect the "eaten" signal to both FoodManager and HUD
-		food.connect("eaten", Callable(food_manager, "_on_food_eaten").bind(food))
-		food.connect("eaten", Callable(hud, "remove_food_ui").bind(food.get_instance_id()))
-
-		print("Food spawned:", food.typeString)
+		#var food = food_manager.add_food(2)  # Register food in FoodManager
+		#food.global_position = get_viewport().get_camera_3d().project_position(get_viewport().get_mouse_position(), 10)
+#
+		##Connect the "eaten" signal to both FoodManager and HUD
+		#food.connect("eaten", Callable(food_manager, "_on_food_eaten").bind(food))
+		#food.connect("eaten", Callable(hud, "remove_food_ui").bind(food.get_instance_id()))
+#
+		#print("Food spawned:", food.typeString)
 		gecko_manager.spawn_rand_gecko()
 		if gecko_manager.current_geckos.size() > 0:
 			hud.connect_gecko(gecko_manager.current_geckos[-1])
